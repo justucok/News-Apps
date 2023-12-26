c0a5f65dd3661eeb60ff48a7b290a2db6ae267f8
justucok <rahmanyw43@gmail.com>
2023-12-26 21:54:02 +0700

    update gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,37 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+//    kotlin("android") version "1.5.0"
 }
 
+//kotlin {
+//    android()
+//
+//    // Gunakan JVM Toolchain untuk menetapkan versi Java
+//    val jvmTarget: String by project
+//    val toolchain = KotlinJvmToolchain(gradle, jvmTarget)
+//
+//    sourceSets {
+//        named("main") {
+//            kotlin.srcDir("src/main/kotlin")
+//            java.srcDir("src/main/java")
+//        }
+//    }
+//
+//    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+//        kotlinOptions.jvmTarget = jvmTarget
+//        useToolchain(toolchain)
+//    }
+//}
 
 android {
     namespace = "com.example.newsapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.newsapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -27,25 +47,35 @@ android {
             )
         }
     }
+
+//    compileSdkVersion(34)
+//
+//    // ...
+
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_1_8
+//        targetCompatibility = JavaVersion.VERSION_1_8
+//    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+//    kotlinOptions {
+//        jvmTarget = "1.8"
+//    }
 }
 
 dependencies {
 
-    implementation("com.squareup.picasso:picasso:2.71828")
-    implementation("com.squareup.retrofit2:retrofit2:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("com.squareup.retrofit2:retrofit2:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0" apply false
 }
